ef2f55bf67c89bb1fc1a4742403ec2f43e8e1f1d
hemanthrishee <88771647+hemanthrishee@users.noreply.github.com>
2023-03-20 22:10:55 +0530

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Server IP address: ");
+        String ip = sc.nextLine();
+        Socket s = new Socket(ip, 5000);
+        PrintWriter bw = new PrintWriter(new OutputStreamWriter(s.getOutputStream()));
+        String temp = "";
+        while (temp.isBlank()) {
+            System.out.print("Enter Name: ");
+            temp = sc.nextLine();
+        }
+        bw.println(temp);
+        bw.flush();
+
+        GroupChat gc = new GroupChat(s);
+        Thread th = new Thread(gc);
+        th.start();
+        temp = "";
+        while (!temp.equals("q")) {
+            temp = sc.nextLine();
+            bw.println(temp);
+            bw.flush();
+        }
+    }
+}
