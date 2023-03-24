b4d28461a03799dbb9121228d43a4fe586504b4e
Manav <Manavbafna87+git@gmail.com>
2023-03-24 17:58:16 +0530

    Adding comments and correcting package statements

--- ClientServer/Server.java
+++ ClientServer/Server.java
@@ -1,13 +1,21 @@
-package MessengerApp.ClientServer;
-
 import java.io.IOException;
 import java.net.*;
 
 public class Server {
+    /**
+     * @param args
+     * @throws IOException
+     */
     public static void main(String[] args) throws IOException {
+
+        // Getting the address of the server
         InetAddress ia = InetAddress.getLocalHost();
         System.out.println(ia.getHostAddress());
+
+        // ServerSocket where Client Socket will connect
         ServerSocket ss = new ServerSocket(5000);
+
+        // This thread will be continuously accepting Clients
         ClientAccepter ca = new ClientAccepter(ss);
         Thread th = new Thread(ca);
         th.start();
