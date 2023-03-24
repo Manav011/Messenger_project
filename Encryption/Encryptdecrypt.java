5277e75fb5a1f3d787414d191953120989ffad9b
Varun Chaudhary <varunc105@gmail.com>
2023-03-24 22:53:52 +0530

    Add files via upload
    Improved Incryption Technology AES(advanced Encryption Standard)

--- Encryption/Encryptdecrypt.java
+++ Encryption/Encryptdecrypt.java
@@ -1,34 +1,40 @@
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import sun.misc.*;
+
+import java.security.Key;
+
 public class Encryptdecrypt {
+    private byte[] key_value;
 
-    public static String encrypt(String s) {
+    Encryptdecrypt(String key) {// Expected key is 16 Characters
+        key_value = key.getBytes();//converting userDecide key into Byte array
+    }
 
-        String encrypt = "";
-
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            ch += 26;
-            encrypt = encrypt + ch;
-
-           
-        }
-        return encrypt;
+    private Key genKey() {// generating key using SecretKeySpec provided by java crypto and choosing AES as a encryption technology
+        Key key = new SecretKeySpec(key_value, "AES");
+        return key;
 
     }
 
-    public static String decrypt(String s) {
-
-        String decrypt = "";
-
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            ch -= 26;
-            decrypt = decrypt + ch;
-
-           
-        }
-        return decrypt;
+    public String encrypt(String s) throws Exception{
+        Key key = genKey();
+        Cipher c = Cipher.getInstance("AES");//getting instance of Cipher in AES mode
+        c.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encrypted_value = c.doFinal(s.getBytes());//encryption done
+        String encrypted_string = new BASE64Encoder().encode(encrypted_value);//coverting to alphanumeric using base64encoder
+        return encrypted_string;
     }
 
+    public String decrypt(String s) throws Exception{
+        Key key = genKey();
+        Cipher c = Cipher.getInstance("AES");
+        c.init(Cipher.DECRYPT_MODE, key);
+        byte[] decoded_value = new BASE64Decoder().decodeBuffer(s);//decoding back to format which AES undrstands
+        byte[] deccrypted_value = c.doFinal(decoded_value);//Decrypting using AES
+        String deccrypted_string = new String(deccrypted_value);
+        return deccrypted_string;
+    }
 
 }
