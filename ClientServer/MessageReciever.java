f263ef6321fa56e1079a43b4bf75da0208566a40
Manav <Manavbafna87+git@gmail.com>
2023-03-24 17:58:16 +0530

    Adding comments and correcting package statements

--- ClientServer/MessageReciever.java
+++ ClientServer/MessageReciever.java
@@ -1,27 +1,32 @@
-package MessengerApp.ClientServer;
 
 import java.io.*;
 import java.net.Socket;
 
-public class MessageReciever implements Runnable{
+public class MessageReciever implements Runnable {
     Socket s;
-    MessageReciever(Socket s)
-    {
+
+    MessageReciever(Socket s) {
         this.s = s;
     }
+
     @Override
-    public void run()
-    {
+    public void run() {
         try {
             String a = "";
+
+            // receving message infinitely until user enters q to quit
             while (!a.equals("q")) {
                 BufferedReader br = null;
                 try {
+
+                    // InputStream for receiving message
                     br = new BufferedReader(new InputStreamReader(s.getInputStream()));
                     a = br.readLine();
                     if (!a.equals("null")) {
                         System.out.println(a);
                     }
+
+                    // handling exception thrown by getInputStream if socket not found
                 } catch (IOException e) {
                     System.out.println();
                 }
