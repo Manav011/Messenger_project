a26dedfabb4a85b5c5b14c229b1c2836a2446185
Varun Chaudhary <varunc105@gmail.com>
2023-04-23 03:14:37 +0530

    databse server added

--- messengerapp/src/main/java/database/Databaseconnection.java
+++ messengerapp/src/main/java/database/Databaseconnection.java
@@ -10,7 +10,8 @@ public class Databaseconnection {
   public  Connection getConnection() {
         try {
 
-            con = DriverManager.getConnection("jdbc:mysql://localhost/messengerapp", "root", "1234");
+//            con = DriverManager.getConnection("jdbc:mysql://localhost/messengerapp", "root", "1234");
+            con = DriverManager.getConnection("jdbc:mysql://43.204.215.33:3306/messengerapp", "root", "1234");
 //            con= DriverManager.getConnection("jdbc:mysql://messenger.cqocdes0lc0n.ap-south-1.rds.amazonaws.com:3306/messengerapp", "admin", "12345678");
 
         } catch (Exception e) {
